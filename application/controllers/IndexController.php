c38334bc5d8f87b3615358eb118371694123aa08
Christophe Boucaut <c.boucautjj@laposte.net>
2012-11-05 16:05:49 +0100

    test commit

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -8,10 +8,16 @@ class IndexController extends Zend_Controller_Action
         /* Initialize action controller here */
     }
 
+    /*
+     * Action réalisée lors de l'arrivée sur le site pour la première fois par l'utilisateur
+     */
     public function indexAction()
     {	
+    	
+    	// instance du formulaire de connexion
     	$form_connexion = new Application_Form_Connexion();
     	
+    	// affichage du formulaire lors de la première visite
     	if(!$this->getRequest()->getPost()){
     		$this->view->assign('form_connexion',$form_connexion);
     	}else{
@@ -26,11 +32,18 @@ class IndexController extends Zend_Controller_Action
     			$identifiant = "";
     			$mdp = "";
     		}
-    		 
+    		
+    		// variable de test renseignant si la connexion à échouée ou non
+    		$test_connexion = false;
+    		
     		// on effectue la connexion
     		if($identifiant!="" && $mdp!=""){
+    			// Instanciation de Zend_Auth
+    			$auth = Zend_Auth::getInstance();
+    		}
     		
-    		}else{
+    		// Si l'identifiant, mdp sont vides ou bien que la connexion a échoué, on réaffiche le formulaire pré-remplit 
+    		if($identifiant!="" || $mdp!="" || !$test_connexion){
     			$form_connexion->populate($data);
     			$this->view->assign('form_connexion',$form_connexion);
     			$this->view->assign('error_connexion', true);
