20b05ca941bf6d3aba0e56ecf1796b98c8cf11af
Christophe Boucaut <c.boucautjj@laposte.net>
2012-11-20 16:09:53 +0100

    Correction de fautes de syntaxes des modèles

--- application/models/TListeSections.php
+++ application/models/TListeSections.php
@@ -1,7 +1,7 @@
 <?php
 class Application_Model_TAeroport extends Zend_Db_Table_Abstract
 {
-	protected $_name = 'liste_section';
+	protected $_name = 'liste_sections';
 	protected $_schema = 'inssetair_db';
 	protected $_primary = 'id_section';
 
